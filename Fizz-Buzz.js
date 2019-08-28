53283b4229420e01206bbda47f449abb24ec97b0
GianMedeiros <54555302+GianMedeiros@users.noreply.github.com>
2019-08-28 17:52:04 -0300

    Update and rename Fizz-Buzz to Fizz-Buzz.js

--- Fizz-Buzz.js
+++ Fizz-Buzz.js
@@ -3,18 +3,15 @@
 function fizzBuzz(data) {
     if (data%3==0 && data%5==0) {
         return "Fizz Buzz";
-    } else {
-        if (data%3==0){
+    } else { if (data%3==0) {
             return "Fizz";    
-        } else {
-            if (data%5==0){
+        } else { if (data%5==0){
                 return "Buzz";
             }  else {
                     return data + "";
-                }
-        }
-    }
-}
+               }
+           }
+      }
 
 /*
  Grupo 2:
